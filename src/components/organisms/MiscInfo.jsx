82d880ade6710907d879cedd42ee33c86019c5d7
Pankaj <coderxhere@gmail.com>
2024-01-17 17:59:40 +0530

    feat: card group ui

--- src/components/organisms/MiscInfo.jsx
+++ src/components/organisms/MiscInfo.jsx
@@ -1,9 +1,16 @@
 import InfoCard from "../molecules/InfoCard";
-
+import style from "./MiscInfo.module.scss";
 const MiscInfo = () => {
   return (
-    <div>
-      <InfoCard />
+    <div className={style.cardGroup}>
+      <div className={style.cardGroup}>
+        <InfoCard />
+        <InfoCard />
+      </div>
+      <div className={style.cardGroup}>
+        <InfoCard />
+        <InfoCard />
+      </div>
     </div>
   );
 };
--- src/components/pages/Weather.jsx
+++ src/components/pages/Weather.jsx
@@ -7,13 +7,15 @@ import MiscInfo from "../organisms/MiscInfo";
 const Weather = () => {
   return (
     <div className={style.mainDiv}>
-      <div className={style.header}>
-        <p className={style.cityName}>Bangalore</p>
-        <Searchbar placeholder={"Enter location..."} />
-      </div>
-      <div>
-        <TemperatureInfo icon={DayIcon} />
-        <MiscInfo />
+      <div className={style.contentDiv}>
+        <div className={style.header}>
+          <p className={style.cityName}>Bangalore</p>
+          <Searchbar placeholder={"Enter location..."} />
+        </div>
+        <div className={style.info}>
+          <TemperatureInfo icon={DayIcon} />
+          <MiscInfo />
+        </div>
       </div>
     </div>
   );
